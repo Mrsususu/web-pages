a5beba193253fb6960201b7e1afb63260e486581
Mrsususu <lisusie@126.com>
2015-12-25 23:58:11 +0800

    js rq2_2
    half of login. Some Ajax function.

--- ykt-project/js/js.js
+++ ykt-project/js/js.js
@@ -17,7 +17,7 @@ function popup() {   //检查cookie,隐藏通知栏，并设置cookie,
         };
     }
 }
-popup();
+popup(); //需求一
 
 function setCookie (key, value, t) {  //设置cookie
     var oDate = new Date();
@@ -39,10 +39,95 @@ function removeCookie (key) {  //删除cookie
     setCookie( key, '', -1 );
 }
 
-function logein() {
-    var ologin = $('j-login');
-    var oAttentioin = $('j-input');
+function serialize (data) {  // 设置参数
+    if (!data) return '';
+    var pairs = [];
+    for (var name in data){
+        if (!data.hasOwnProperty(name)) continue;
+        if (typeof data[name] === 'function') continue;
+        var value = data[name].toString();
+        name = encodeURIComponent(name);
+        value = encodeURIComponent(value);
+        pairs.push(name + '=' + value);
+    }
+    return pairs.join('&');
+}
+ 
+function get(url,options,callback) {  //get方法
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == 4) {
+            if((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
+                callback(xhr.responseText);
+            } else {
+                alert ( 'request failed : ' + xhr.status);
+            }
+        }
+    };
+    xhr.open('get', url + '?' + serialize(options), true);
+    xhr.send(null);
+}
+
+function getElementsByClassName(element, names) {  //获取class元素
+    if (element.getElementsByClassName){
+        return element.getElementsByClassName(names);
+    } else {
+        var elements = element.getElementsByTagName('*');
+        var result=[];
+        var element,
+            classNameStr,
+            flag,
+            name;
+
+        names = names.split(' ');
+        for( var i = 0; element = elements[i]; i++) {
+            classNameStr = ' ' + element.className + ' ';
+            flag = true;
+            for( var j = 0; name = names[j]; j++) {
+                if(classNameStr.indexOf(' ' + name + ' ') == -1){
+                    flag == false;
+                    break;
+                }
+            }
+            if(flag) {
+                result.push(element);
+            }            
+        }
+        return result;
+    }
 
 }
 
 
+function login() {
+    var ologin = $('j-login');
+    var oAttentioin = $('j-input');
+    var oPopuplog = getElementsByClassName(ologin,'popuplog');
+    var oClose = getElementsByClassName(ologin,'close');
+    var oInput = ologin.getElementsByTagName('input');
+    var oLabel = ologin.getElementsByTagName('label');
+    var oButton = getElementsByClassName(ologin,'submit');
+    var oCancel = getElementsByClassName(ologin,'cancel');
+
+    function focus(i) {  //弹窗输入时，提示文字隐藏
+        oInput[i+1].onfocus = function() {
+            oLabel[i].style.display = 'none';
+        }
+        oInput[i+1].onblur = function() {
+            if( this.value ===''){
+                oLabel[i].style.display = 'block';
+            }
+        }
+    }
+    focus(0);
+    focus(1);
+
+    oAttentioin.onclick = function() {
+        if (!getCookie('loginSuc')){
+            oPopuplog[0].style.display = 'block';
+            setCookie( 'followSuc', true, 36500 );
+        }
+    }
+}
+
+login();
